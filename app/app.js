4bbb302035fb021f81d3e5561b222d98ca672e51
gabrielajasnosz <gabriela.jasnosz@gmail.com>
2021-05-17 08:57:05 +0200

    lab9

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@ import config from './config';
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
+import routes from './REST/routes';
 
 const app = express();
 app.use(express.static(__dirname + '/public'));
@@ -14,6 +16,8 @@ app.use(bodyParser.json({limit: '2048kb'}));
 app.use(express.static('public'));
 
 app.use(cors());
+routes(app);
+
 
 app.get('/*', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
@@ -22,3 +26,25 @@ app.get('/*', function (req, res) {
 app.listen(config.port, function () {
   console.info(`Server is running at ${config.port}`)
 });
+
+
+mongoose.connect(config.databaseUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}, (error) => {
+  if (error) {
+    console.error(error);
+  }
+  else {
+    console.info('Connect with database established');
+  }
+});
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(function () {
+    console.error('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
+
